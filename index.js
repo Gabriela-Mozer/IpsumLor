abac7d140b4bf7ca99e59bad932c5e542a99f295
Gabriela Mozer <gabrielamozer1966@gmail.com>
2021-12-23 14:17:39 +0100

    first version of desktop

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-//const extraClass = document.createElement('class');
-const socials = document.createElement('socials');
 
-socials.style.background ='url(imgs/github.svg) no-repeat';
-let smallSize = 840 + 'px';
-
-const appearElement =function(){
-    if(window.innerWidth < smallSize){
-        socials.style.display ='none';
-    }
-}
