b4619ba3ea8bbb66e05cf89cf7f0ccf7b5dd58d6
Gabriela Mozer <gabrielamozer1966@gmail.com>
2021-12-25 21:08:44 +0100

    adding observer func

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const  main = document.querySelectorAll('.great');
+
+const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry =>{
+        entry.target.classList.toggle('show', entry.isIntersecting)
+        if (entry.isIntersecting)observer.unobserve(entry.target)
+    })
+    
+},{
+    threshold: 0.5,
+
+});
+
+main.forEach(great=>{
+    observer.observe(great);
+})
+
