6d90f66d9afc04a1847db66fd21b55e7a58f3b0e
Gabriela Mozer <gabrielamozer1966@gmail.com>
2021-12-28 16:27:28 +0100

    making observer in offers & create querySelectorbutton

---
+++ dist/index.dev.js
@@ -0,0 +1,145 @@
+"use strict";
+
+var main = document.querySelector(".great");
+
+var callback = function callback(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target); //console.log(entries)
+    } else {
+      entry.target.classList.remove("show"); //observer.unobserve(entry.target);
+    }
+  });
+};
+
+var options = {
+  rootMargin: "20px" //threshold: 0.3,
+
+};
+var myObserver = new IntersectionObserver(callback, options);
+myObserver.observe(main);
+var text = document.querySelector(".text__article");
+
+var calltext = function calltext(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show"); //observer.unobserve(entry.target)
+    } else {
+      entry.target.classList.remove("show");
+    }
+
+    var options = {
+      rootMargin: "20px",
+      threshold: 0.15
+    };
+  });
+};
+
+var textObserver = new IntersectionObserver(calltext, options);
+textObserver.observe(text);
+var offersheading = document.querySelector('.offers__container--heading');
+
+var calloffer = function calloffer(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // observer.unobserve(entry.target)
+      // console.log(entry.target)
+    } else {
+      entry.target.classList.remove('show'); //observer.unobserve(entry.target)
+    }
+
+    ;
+    var options = {
+      rootMargin: '0px',
+      threshold: 0.15
+    };
+  });
+};
+
+var offerObserver = new IntersectionObserver(calloffer, options);
+offerObserver.observe(offersheading);
+var offersheadingrich = document.querySelector('.offers__container--headingFirst');
+
+var callofferrich = function callofferrich(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // observer.unobserve(entry.target)
+      // console.log(entry.target)
+    } else {
+      entry.target.classList.remove('show'); //observer.unobserve(entry.target)
+    }
+
+    ;
+    var options = {
+      rootMargin: '0px',
+      threshold: 0.15
+    };
+  });
+};
+
+var offerrichObserver = new IntersectionObserver(callofferrich, options);
+offerObserver.observe(offersheadingrich);
+var paragraphfirst = document.querySelector('.offers__container--paragraph--first');
+
+var callbackparagraphFirst = function callbackparagraphFirst(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // observer.unobserve(entry.target)
+      // console.log(entry.target)
+    } else {
+      entry.target.classList.remove('show'); //observer.unobserve(entry.target)
+    }
+
+    ;
+    var options = {
+      rootMargin: '0px',
+      threshold: 0.15
+    };
+  });
+};
+
+var paragraphFirstObserver = new IntersectionObserver(callbackparagraphFirst, options);
+paragraphFirstObserver.observe(paragraphfirst);
+var paragraphsec = document.querySelector('.offers__container--paragraph');
+
+var callbackparagraphsec = function callbackparagraphsec(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // observer.unobserve(entry.target)
+      // console.log(entry.target)
+    } else {
+      entry.target.classList.remove('show'); //observer.unobserve(entry.target)
+    }
+
+    ;
+    var options = {
+      rootMargin: '0px',
+      threshold: 0.15
+    };
+  });
+};
+
+var paragraphSecObserver = new IntersectionObserver(callbackparagraphsec, options);
+paragraphSecObserver.observe(paragraphsec);
+var button = document.querySelector('.offers__container--price'); //const button = document.getElementsByName('offers');
+
+var callbackbutton = function callbackbutton(entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // observer.unobserve(entry.target)
+      // console.log(entry.target)
+    } else {
+      entry.target.classList.remove('show'); //observer.unobserve(entry.target)
+    }
+
+    ;
+    var options = {
+      rootMargin: '0px',
+      threshold: 0.15
+    };
+  });
+};
+
+var buttonObserver = new IntersectionObserver(callbackbutton, options);
+buttonObserver.observe(button);
--- index.js
+++ index.js
@@ -1,31 +1,158 @@
-const main = document.querySelector('.great');
+const main = document.querySelector(".great");
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+      //console.log(entries)
+    } else {
+      entry.target.classList.remove("show");
+      //observer.unobserve(entry.target);
+    }
+  });
+};
+const options = {
+  rootMargin: "20px",
+  //threshold: 0.3,
+};
+
+const myObserver = new IntersectionObserver(callback, options);
+myObserver.observe(main);
+
+const text = document.querySelector(".text__article");
+
+const calltext = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      //observer.unobserve(entry.target)
+    } else {
+      entry.target.classList.remove("show");
+    }
+    const options = {
+      rootMargin: "20px",
+      threshold: 0.15,
+    };
+  });
+};
+const textObserver = new IntersectionObserver(calltext, options)
+textObserver.observe(text);
 
 
+const offersheading = document.querySelector('.offers__container--heading');
 
-const observer = new IntersectionObserver(entries =>{
+
+const calloffer = (entries, observer)=>{
   entries.forEach(entry =>{
-    entry.target.classList.toggle('show', entry.isIntersecting)
-    //if(entry.isIntersecting) observer.unobserve(entry.target)
-  },{
-    threshold :0.01,
-   // rootMargin : '10px'
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+     // observer.unobserve(entry.target)
+     // console.log(entry.target)
+    }
+    else{
+      entry.target.classList.remove('show')
+      //observer.unobserve(entry.target)
+    };
+    const options={
+      rootMargin: '0px',
+      threshold:0.15
+    }
   })
-  console.log(entries)
-})
-const lastObserver = new IntersectionObserver(entries =>{
-  const lastOne = entries
-  if(!lastCard.isIntersecting) return
-  loadnewCard()
-})
+}
+const offerObserver = new IntersectionObserver(calloffer,options);
+offerObserver.observe(offersheading)
 
-observer.observe(main)
 
-// function loadnewCard(){
-// for(let i =0; i<10; i++){
-//   const card = document.createElement('div')
-//   card.textContent = 'nówka'
-//   card.classList.add('main');
-//   observer.observe(card)
-//   mainContainer.append(card)
-// }
-// }
+const offersheadingrich = document.querySelector('.offers__container--headingFirst');
+
+
+const callofferrich = (entries, observer)=>{
+  entries.forEach(entry =>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+     // observer.unobserve(entry.target)
+     // console.log(entry.target)
+    }
+    else{
+      entry.target.classList.remove('show')
+      //observer.unobserve(entry.target)
+    };
+    const options={
+      rootMargin: '0px',
+      threshold:0.15
+    }
+  })
+}
+const offerrichObserver = new IntersectionObserver(callofferrich,options);
+offerObserver.observe(offersheadingrich)
+
+
+const paragraphfirst = document.querySelector('.offers__container--paragraph--first');
+
+
+const callbackparagraphFirst = (entries, observer)=>{
+  entries.forEach(entry =>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+     // observer.unobserve(entry.target)
+     // console.log(entry.target)
+    }
+    else{
+      entry.target.classList.remove('show')
+      //observer.unobserve(entry.target)
+    };
+    const options={
+      rootMargin: '0px',
+      threshold:0.15
+    }
+  })
+}
+const paragraphFirstObserver= new IntersectionObserver(callbackparagraphFirst,options);
+paragraphFirstObserver.observe(paragraphfirst)
+
+const paragraphsec = document.querySelector('.offers__container--paragraph');
+
+
+const callbackparagraphsec = (entries, observer)=>{
+  entries.forEach(entry =>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+     // observer.unobserve(entry.target)
+     // console.log(entry.target)
+    }
+    else{
+      entry.target.classList.remove('show')
+      //observer.unobserve(entry.target)
+    };
+    const options={
+      rootMargin: '0px',
+      threshold:0.15
+    }
+  })
+}
+const paragraphSecObserver= new IntersectionObserver(callbackparagraphsec,options);
+paragraphSecObserver.observe(paragraphsec)
+
+const button = document.querySelector('.offers__container--price');
+//const button = document.getElementsByName('offers');
+
+const callbackbutton = (entries, observer)=>{
+  entries.forEach(entry =>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+     // observer.unobserve(entry.target)
+     // console.log(entry.target)
+    }
+    else{
+      entry.target.classList.remove('show')
+      //observer.unobserve(entry.target)
+    };
+    const options={
+      rootMargin: '0px',
+      threshold:0.15
+    }
+  })
+}
+const buttonObserver= new IntersectionObserver(callbackbutton,options);
+buttonObserver.observe(button)
